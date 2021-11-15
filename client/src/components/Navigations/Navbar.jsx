8c84d4bb0a47ed6361403a1087b6f692c4a06b95
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-11-15 20:07:20 +0100

    Connecton with MongoDB Atlas and Add scss file with react

---
+++ client/src/components/Navigations/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FiCoffee} from 'react-icons/fi';
+
+const Navbar = () => {
+    return (
+        <div>
+            <a href='#' className="logo">Post</a>
+        </div>
+    )
+}
+
+export default Navbar;
